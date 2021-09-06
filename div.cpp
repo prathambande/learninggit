637836fd3274d3875c04773964c8742555b87efa
prathambande <pratham0802@gmail.com>
2021-09-06 18:44:28 +0530

    working division code has been added

--- div.cpp
+++ div.cpp
@@ -1,2 +1,27 @@
-//under construction 
+#include<iostream>
+using namespace std;
+
+float division(int a, int b)
+{
+    if(b == 0)
+    {
+        throw(0);
+    }
+    return (float)a/b;
+}
+
+int main()
+{
+    int a,b;
+    cin >> a >> b;
+    
+    try{
+        cout << division(a,b) << endl;
+    }
+    catch(int e)
+    {
+        cout << "Denominator cannot be 0" << endl;
+    }
+    return 0;
+}
 
